908088af9df3fc53cf801f94f53e62dd0b5d7bca
Mehmet Talha AYAR <mehmettalhaayar61@gmail.com>
2025-05-14 15:37:23 +0300

    add digits has been compeleted

---
+++ java/Easy/258. Add Digits/AddDigits.java
@@ -0,0 +1,15 @@
+public class AddDigits {
+
+
+    public static void main(String arg[]){
+        Solution solution = new Solution();
+
+        System.out.println(solution.addDigits(38));
+        System.out.println(solution.addDigits(2));
+        System.out.println(solution.addDigits(211));
+        System.out.println(solution.addDigits(24789));
+
+
+    }
+    
+}
