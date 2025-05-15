fbc3808947ed13bcb75c21ba143a1d4c5c7a69f7
Mehmet Talha AYAR <mehmettalhaayar61@gmail.com>
2025-05-15 23:17:55 +0300

    length of last word has been completed.

---
+++ java/Easy/58. Length of Last Word/LengthOfLastWord.java
@@ -0,0 +1,14 @@
+public class LengthOfLastWord {
+    
+
+
+    public static void main(String arg[]){
+        
+        Solution solution = new Solution();
+
+        System.out.println(solution.lengthOfLastWord("Hello World"));
+        System.out.println(solution.lengthOfLastWord("   fly me   to   the moon  "));
+        System.out.println(solution.lengthOfLastWord("luffy is still joyboy"));
+        
+    }
+}
---
+++ java/Easy/58. Length of Last Word/Solution.java
@@ -0,0 +1,13 @@
+
+public class Solution {
+
+
+
+    public int lengthOfLastWord(String s) {
+        
+        String[] stringArray = s.trim().split(" ");
+
+        return stringArray[stringArray.length-1].length();
+    }
+
+}
